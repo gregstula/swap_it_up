683b589e63f6664db5baa1d7c60d44810d93ce71
Gregory D Stula <gregstula.dev@icloud.com>
2017-02-23 07:47:03 -0500

    Add password validation to registration
    Set score to zero on new user creation

--- web/models/user.ex
+++ web/models/user.ex
@@ -19,11 +19,47 @@ defmodule SwapItUp.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:username, :email, :password_hash, :score])
-    |> validate_required([:username, :email, :password_hash])
+    |> cast(params, [:username, :email, :score])
+    |> validate_required([:username, :email])
     |> validate_length(:username, min: 4, max: 20)
     |> validate_format(:email, ~r/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
     |> unique_constraint(:username)
     |> unique_constraint(:email)
   end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> password_changeset(params)
+    |> start_new_score()
+  end
+  
+  def password_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, [:password, :password_confirmation])
+    |> validate_required([:password, :password_confirmation])
+    |> validate_length(:password, min: 6, max: 100)
+    #    |> validate_password_confirmation()
+    |> put_pw_hash()
+  end 
+
+  defp put_pw_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pw}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pw))
+      _ ->
+        changeset
+    end
+  end
+
+  defp start_new_score(changeset) do
+  case changeset do
+      %Ecto.Changeset{valid?: true} ->
+        put_change(changeset, :score, 0)
+      _ ->
+        changeset
+    end
+  end
 end
+
